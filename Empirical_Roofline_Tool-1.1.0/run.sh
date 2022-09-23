3d55f97583cade07b5f2ef305c2963d5dd9dad25
ravenouse <zhwa3087@colorado.edu>
2022-09-23 14:21:20 -0600

    initial commit for lab5

---
+++ Empirical_Roofline_Tool-1.1.0/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=00:05:00
+#SBATCH --partition=shas-testing
+#SBATCH --output=ex01-%j.out
+
+
+# Load the modules
+module purge
+module load intel
+module load impi 
+
+# Run the application
+/projects/zhwa3087/CSCI5576/cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0/ert \
+/projects/zhwa3087/CSCI5576/cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0/Config/config.lab_05.01
